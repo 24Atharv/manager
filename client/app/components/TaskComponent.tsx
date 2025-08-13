7df9f9ae9ee6b5fa700158ef80bf1a29cc2b6ad0
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-13 17:33:16 +0530

    Taskcomponent Icon updated

--- client/app/components/TaskComponent.tsx
+++ client/app/components/TaskComponent.tsx
@@ -7,26 +7,26 @@ import { TodoIcon } from "../Icons/TodoIcon"
 
 export const TaskCompnent = () => {
     return <div>
-        <div className="border border-gray-800 rounded-lg">
-            <div className="text-black flex gap-4 p-4">
-                <span>
+        <div className="border border-gray-800 rounded-lg mt-6">
+            <div className="text-black flex gap-9 p-4">
+                <span className="flex gap-2 items-center py-2 px-4 rounded bg-orange-400">
                     <SchedularIcon />
                     Scheduler
                 </span>
-                <span>
+                <span className="flex gap-1 items-center">
                     <ProductivityIcon />
                     Productivity
                 </span>
-                <span>
+                <span className="flex gap-1 items-center">
                     <TodoIcon />
                     Todo</span>
-                <span>
+                <span className="flex gap-1 items-center">
                     <TimelineIcon />
                     Timeline</span>
-                <span>
+                <span className="flex gap-1 items-center">
                     <CalenderIcon />
                     Calender</span>
-                <span>
+                <span className="flex gap-1 items-center">
                     <ProjectIcon />
                     Projects</span>
             </div>
