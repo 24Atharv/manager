5eb83de5d43619202dfbc1475d9f66db1bc49ee9
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-13 18:22:11 +0530

    TaskComponent added

--- client/app/components/TaskComponent.tsx
+++ client/app/components/TaskComponent.tsx
@@ -30,6 +30,68 @@ export const TaskCompnent = () => {
                     <ProjectIcon />
                     Projects</span>
             </div>
+            <div className="m-3 flex flex-wrap gap-6 w-fit">
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2 text-black p-5 rounded w-66 border border-gray-300">
+                    <div className="flex gap-2">
+                        <span className="px-4 py-1 w-fit rounded-2xl bg-blue-100 text-blue-800">Time slot</span>
+                        <span className="px-5 py-1 rounded-lg bg-blue-100 text-blue-800">Edit</span>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <span className="text-gray-600">10:00 AM - 12:00 PM</span>
+                        <span className="text-black font-bold">Task1</span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 }
