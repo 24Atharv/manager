1e321adcffdfe574cbf6e5ab0024d4c4d840c8ae
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-13 18:45:58 +0530

    footer create

--- client/app/components/Footer.tsx
+++ client/app/components/Footer.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image"
 import leafSvg from '../assets/leaf.svg'
+import { GithubIcon } from "../Icons/GithubIcon"
+import { TwitterIcon } from "../Icons/TwitterIcon"
 
 export const Footer = () => {
     return <div className="mt-9">
         <hr className="border border-gray-800" />
+        <div className="flex gap-3 justify-between mt-2 items-center">
         <Image src={leafSvg} alt="leafIcon" className="w-14 h-14" />
         <span className="text-gray-800">© 2025 Tymeline, LLC. All rights reserved.</span>
+        <span className="flex gap-1">
+            <GithubIcon />
+            <TwitterIcon />
+        </span>
+        </div>
     </div>
 }
