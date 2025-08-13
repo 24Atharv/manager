34bd0b60bd1e1ebe8a0964ff7f7a766ae33d84eb
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-14 00:57:22 +0530

    Link card add

--- client/app/components/Middle.tsx
+++ client/app/components/Middle.tsx
@@ -1,20 +1,27 @@
 import { MiddleCard } from "./MiddleCard"
+import { MiddleSlider } from "./MiddleSlider"
+import { MiddleSpan } from "./MiddleSpan"
 
 export const Middle = () => {
-    return <div className="flex justify-center">
-        <div className="flex justify-around gap-10 mt-8 items-center text-black">
-            <div className="flex flex-col gap-6 w-126">
-                <span className="text-3xl font-bold">Everything you need to stay organized, with powerful integrations built-in.</span>
-                <span className="text-lg text-gray-600">Streamline your workflow with intelligent task management, seamless time tracking, and organized link collections that sync across all your devices.</span>
-                <span className="text-lg text-gray-600">Whether you're a developer, designer, or entrepreneur, tymeline adapts to your unique productivity needs with customizable workflows and smart automation.</span>
-                <button className="px-5 py-2 rounded bg-orange-400 w-fit">Start Now</button>
+    return <div className="flex flex-col">
+        <div className="flex justify-center">
+            <div className="flex justify-around gap-10 mt-8 items-center text-black">
+                <div className="flex flex-col gap-6 w-126">
+                    <span className="text-3xl font-bold">Everything you need to stay organized, with powerful integrations built-in.</span>
+                    <span className="text-lg text-gray-600">Streamline your workflow with intelligent task management, seamless time tracking, and organized link collections that sync across all your devices.</span>
+                    <span className="text-lg text-gray-600">Whether you're a developer, designer, or entrepreneur, tymeline adapts to your unique productivity needs with customizable workflows and smart automation.</span>
+                    <button className="px-5 py-2 rounded bg-orange-400 w-fit">Start Now</button>
+                </div>
+                <div className="w-[2px] h-120 bg-gray-700"></div>
+                <div className="w-136">
+                    <MiddleCard />
+                </div>
             </div>
-            <div className="w-[2px] h-120 bg-gray-700"></div>
-            <div className="w-136">
-                <MiddleCard />
-            </div>
-
         </div>
+        <div className="flex justify-center items-center text-center text-black text-4xl">
+        <MiddleSpan />
+        </div>
+        <MiddleSlider />
     </div>
 }
 
---
+++ client/app/components/MiddleSpan.tsx
@@ -0,0 +1,25 @@
+export const MiddleSpan = () => {
+    return <div className="mt-18 flex flex-col gap-10 items-center text-center justify-center">
+        <span>Wave goodbye to
+            <span className="text-red-400"> disorganized workflows</span>
+        </span>
+        <span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-14">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+            </svg>
+        </span>
+        <span className="text-center text-2xl text-gray-400 w-203">
+            Transform chaos into clarity with our intelligent productivity platform. Streamline your workflow, boost collaboration, and achieve more with less effort.
+        </span>
+        <div className="flex gap-1">
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+            <div className="bg-gray-600 w-[12px] h-3 rounded-2xl"></div>
+        </div>
+        <span className="text-4xl">Features Showcase</span>
+    </div>
+}
+
