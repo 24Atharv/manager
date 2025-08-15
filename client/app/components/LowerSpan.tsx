c3f2a6448388e84af2d9613dc287856a007e21c9
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-15 10:00:36 +0530

    LowerSpan responsive

--- client/app/components/LowerSpan.tsx
+++ client/app/components/LowerSpan.tsx
@@ -1,6 +1,6 @@
 export const LowerSpan = () => {
     return <div className="text-black flex flex-col gap-4 text-center items-center mt-14">
-        <span className="text-4xl">Stay Organized & Productive</span>
-        <span className="w-202 text-lg text-gray-500">Simplify your workflow with Quick Access, Scheduling, Productivity Tracking, Timelines, Calendar, Projects, Habits, Kanban, and more.</span>
+        <span className="text-3xl sm:text-4xl">Stay Organized & Productive</span>
+        <span className="text-sm sm:w-202 sm:text-lg text-gray-500">Simplify your workflow with Quick Access, Scheduling, Productivity Tracking, Timelines, Calendar, Projects, Habits, Kanban, and more.</span>
     </div>
 }
