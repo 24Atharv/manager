351624a94bbfa731661fb883b7e5b65102995945
Atharv Nandurkar <atharvnandurkar@gmail.com>
2025-08-15 08:06:44 +0530

    navbar responsiveness

--- client/app/components/Dashboard.tsx
+++ client/app/components/Dashboard.tsx
@@ -5,13 +5,15 @@ import { Footer } from "./Footer"
 import { Middle } from "./Middle"
 
 export const Dashboard = () => {
-    return <div className="">
-        <div className="py-5 px-15">
-            <Navbar />
-            <HeroSpan />
-            <TaskCompnent />
-            <Middle />
-            <Footer />
+    return <div className="w-full bg-white">
+        <div className="w-full max-w-full md:max-w-[1024px] mx-auto px-4">
+            <div className="py-5 px-15">
+                <Navbar />
+                <HeroSpan />
+                <TaskCompnent />
+                <Middle />
+                <Footer />
+            </div>
         </div>
     </div>
 }
--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -5,11 +5,11 @@ export const Navbar = () => {
     return <div className="border border-gray-100 shadow-2xs rounded-lg p-3 flex items-center justify-between hover:shadow-2xl">
         <div className="flex items-center">
             <LeafIcon />
-            <span className="text-gray-900 text-2xl">tymeline</span>
+            <span className="hidden sm:inline sm:text-gray-900 sm:text-2xl">tymeline</span>
         </div>
-        <div className="flex items-center gap-6 cursor-pointer">
+        <div className="flex items-center gap-2 sm:gap-5 cursor-pointer">
             <SunIcon />
-            <span className="bg-white border border-gray-400 text-gray-900 rounded py-2 px-4 text-xl hover:shadow-2xl">
+            <span className="hidden sm:inline bg-white border border-gray-400 text-gray-900 rounded py-2 px-4 text-xl hover:shadow-2xl">
                 Log in
             </span>
             <span className="bg-orange-400 border border-gray-400 rounded py-2 px-4 text-xl font-semibold">
